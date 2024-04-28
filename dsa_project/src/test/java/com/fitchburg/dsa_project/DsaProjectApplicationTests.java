34d5366d0416c8924d616007610b369079346e4e
KOMARA CHANDRA PRASAD <komarachandraprasad@gmail.com>
2024-04-28 19:41:51 -0400

    projectcreated

---
+++ dsa_project/src/test/java/com/fitchburg/dsa_project/DsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitchburg.dsa_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
